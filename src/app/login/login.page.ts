eb6733eaf85a9bd1c8e7dc655ae77bbebc1d42b8
Daniel Ramirez <dani_0318@hotmail.com>
2022-06-15 23:54:12 -0500

    clase 15 validar input de formulario reactivo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  loginForm: FormGroup;
+  validation_messages = {
+    email: [
+      {type: "required", message: "El email es requerido"},
+      {type: "pattern", message: "No es un email válido"}
+    ],
+    password: [
+      {type: 'required', message: 'El password es requerido'},
+      {type: 'minLength', message: 'Tamaño minimo 5 caracteres'}
+    ],
+  };
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+      ])),
+      password: new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ]))
+    });
+   }
 
   ngOnInit() {
   }
+
+  loginUser(loginForm){}
 
 }
