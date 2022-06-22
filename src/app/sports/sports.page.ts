04b87dc02c080490a91a4228872c693968986618
Daniel Ramirez <dani_0318@hotmail.com>
2022-06-22 00:41:12 -0500

    clase 36 Crear la página Sport y uso de geolocalizando y trazado de recorrido

--- src/app/sports/sports.page.ts
+++ src/app/sports/sports.page.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-sports',
   templateUrl: './sports.page.html',
   styleUrls: ['./sports.page.scss'],
 })
-export class SportsPage implements OnInit {
+export class SportsPage {
+
+  currentCenter: any;
+  coordinates: google.maps.LatLngLiteral[] = [];
+  defaultZoom = 14;
+  center: google.maps.LatLngLiteral;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.getCurrentPosition();
+    this.watchPosition();
+  }
+
+
+  async getCurrentPosition(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.currentCenter = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    };
+    this.center = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    }
+  }
+
+  watchPosition() {
+    Geolocation.watchPosition({}, position=> {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      this.coordinates.push ({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+    });
   }
 
 }
